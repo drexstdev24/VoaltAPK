f3b24791e6e7dcecca0406b04fb49666327df5f5
Dexst <161962924+drexstdev24@users.noreply.github.com>
2024-12-04 02:14:41 +0000

    compiler

--- video/lib/build.gradle.kts
+++ video/lib/build.gradle.kts
@@ -10,4 +10,8 @@ dependencies {
   implementention(project(":core-util"))
   implementention(libs.libssignal.android)
   implementention(libs.google.guava.android)
+
+  implementention(libs.bundles.mp4parser) {
+    exclude(group = "junit", module = "junit")
+  }
 }
